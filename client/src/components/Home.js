20520dfd4640a8c6254a14efc13fa25bd1d91ca4
sain06533 <70563198+sain06533@users.noreply.github.com>
2024-08-06 08:55:00 +0530

    Revert "added ad carousel to the landing page"
    This reverts commit f30fabb3b1184180322ba345b4037d4be354f8e1.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useNavigate } from "react-router-dom";
 import './Home.css';
 import jobProviderImage from './job-provider.jpg';
